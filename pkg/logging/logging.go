a6cc595483cf30c7a08cd916fd5e0bd8292c972c
Hasan Turken <turkenh@gmail.com>
2019-10-10 16:43:45 +0300

    Add zapLogger with option to disable stacktraces
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -19,7 +19,12 @@ limitations under the License.
 package logging
 
 import (
+	"os"
+
 	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
+	runtimezap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 	runtimelog "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
 
@@ -34,7 +39,26 @@ var (
 	// Logger is the base logger used by Crossplane. It delegates to another
 	// logr.Logger. You *must* call SetLogger to get any actual logging.
 	Logger = logger.WithName("crossplane")
+
+	// ZapLogger is a Logger implementation.
+	ZapLogger = zapLogger
 )
+
+// ZapLogger is a Logger implementation from controller runtime.
+// If development is true, a Zap development config will be used
+// (stacktraces on warnings, no sampling), otherwise a Zap production
+// config will be used (stacktraces on errors, sampling).
+// If disableStacktrace is true, stacktraces enabled on fatals
+// independent of config.
+func zapLogger(development bool, disableStacktrace bool) logr.Logger {
+	zl := runtimezap.RawLoggerTo(os.Stderr, development)
+
+	if disableStacktrace {
+		zl = zl.WithOptions(zap.AddStacktrace(zap.FatalLevel))
+	}
+
+	return zapr.NewLogger(zl)
+}
 
 // SetLogger sets a concrete logging implementation for all deferred Loggers.
 func SetLogger(l logr.Logger) {
