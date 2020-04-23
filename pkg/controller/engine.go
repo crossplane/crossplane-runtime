357942c02d95102cc8f8d61d7460a78fa4613083
Nic Cope <negz@rk0n.org>
2020-04-22 19:08:16 -0700

    Add a "controller engine" that manages the lifecycles of controllers
    A controller engine is somewhat like a controller "sub-manager", in that it's
    effectively a group of controllers. Unlike a typical controller manager, the
    lifecycle of the controllers an engine manages are not coupled to the lifecycle
    of the engine itself. An engine may be used by a parent controller to start and
    stop child controllers in accordance with configuration provided by the custom
    resource that the parent controller watches.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- pkg/controller/engine.go
+++ pkg/controller/engine.go
@@ -14,35 +14,87 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package controller provides utilties for working with controllers.
 package controller
 
 import (
-	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"sync"
+
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
+// Error strings
+const (
+	errCreateCache      = "cannot create new cache"
+	errCreateController = "cannot create new controller"
+	errCrashCache       = "cache error"
+	errCrashController  = "controller error"
+	errWatch            = "cannot setup watch"
+)
+
+// A NewCacheFn creates a new controller-runtime cache.
+type NewCacheFn func(cfg *rest.Config, o cache.Options) (cache.Cache, error)
+
+// A NewControllerFn creates a new controller-runtime controller.
+type NewControllerFn func(name string, m manager.Manager, o controller.Options) (controller.Controller, error)
+
+// The default new cache and new controller functions.
+var (
+	DefaultNewCacheFn      NewCacheFn      = cache.New
+	DefaultNewControllerFn NewControllerFn = controller.NewUnmanaged
+)
+
+// An Engine manages the lifecycles of controller-runtime controllers (and their
+// caches). The lifecycles of the controllers are not coupled to lifecycle of
+// the engine, nor to the lifecycle of the controller manager it uses.
+type Engine struct {
+	mgr manager.Manager
+
+	started map[string]chan struct{}
+	errors  map[string]error
+	mx      sync.RWMutex
+
+	newCache NewCacheFn
+	newCtrl  NewControllerFn
+}
+
+// An EngineOption configures an Engine.
 type EngineOption func(*Engine)
 
-func WithLogger(l logging.Logger) EngineOption {
+// WithNewCacheFn may be used to configure a different cache implementation.
+// DefaultNewCacheFn is used by default.
+func WithNewCacheFn(fn NewCacheFn) EngineOption {
 	return func(e *Engine) {
-		e.log = l
+		e.newCache = fn
 	}
 }
 
+// WithNewControllerFn may be used to configure a different controller
+// implementation. DefaultNewControllerFn is used by default.
+func WithNewControllerFn(fn NewControllerFn) EngineOption {
+	return func(e *Engine) {
+		e.newCtrl = fn
+	}
+}
+
+// NewEngine produces a new Engine.
 func NewEngine(mgr manager.Manager, o ...EngineOption) *Engine {
 	e := &Engine{
-		mgr:     mgr,
-		started: map[string]chan struct{}{},
+		mgr: mgr,
 
-		log: logging.NewNopLogger(),
+		started: make(map[string]chan struct{}),
+		errors:  make(map[string]error),
+
+		newCache: DefaultNewCacheFn,
+		newCtrl:  DefaultNewControllerFn,
 	}
 
 	for _, eo := range o {
@@ -52,104 +104,103 @@ func NewEngine(mgr manager.Manager, o ...EngineOption) *Engine {
 	return e
 }
 
-type Engine struct {
-	mgr     manager.Manager
-	started map[string]chan struct{}
-
-	log logging.Logger
-}
-
+// IsRunning indicates whether the named controller is running - i.e. whether it
+// has been started and does not appear to have crashed.
 func (e *Engine) IsRunning(name string) bool {
-	stop, started := e.started[name]
-	if !started {
-		return false
-	}
+	e.mx.RLock()
+	defer e.mx.RUnlock()
 
-	// Nothing ever writes to the stop channel. If we can read from it it's
-	// closed and therefore the controller is no longer running. If we can't
-	// immediately read from it, the channel is not closed, and therefore the
-	// controller is still running.
-	select {
-	case <-stop:
-		return false
-	default:
-		return true
-	}
+	_, running := e.started[name]
+	return running
 }
 
+// Err returns any error encountered by the named controller. The returned error
+// is always nil if the named controller is running.
+func (e *Engine) Err(name string) error {
+	e.mx.RLock()
+	defer e.mx.RUnlock()
+
+	return e.errors[name]
+}
+
+// Stop the named controller.
 func (e *Engine) Stop(name string) {
-	stop, exists := e.started[name]
-	if !exists {
+	e.done(name, nil)
+}
+
+func (e *Engine) done(name string, err error) {
+	e.mx.Lock()
+	defer e.mx.Unlock()
+
+	stop, ok := e.started[name]
+	if ok {
+		close(stop)
+		delete(e.started, name)
+	}
+
+	// Don't overwrite the first error if done is called multiple times.
+	if e.errors[name] != nil {
 		return
 	}
-	close(stop)
-	delete(e.started, name)
+	e.errors[name] = err
 }
 
-// TODO(negz): Accept alternative EventHandlers
-// TODO(negz): Accept a different map of object kind to EventHandler?
-
-// Source, eventhandler, predicates.
-// Source is fixed-ish. Different object, same cache.
-
-type For struct {
-	Kind       runtime.Object
-	Handler    handler.EventHandler
-	Predicates []predicate.Predicate
+// Watch an object.
+type Watch struct {
+	kind       runtime.Object
+	handler    handler.EventHandler
+	predicates []predicate.Predicate
 }
 
-// TODO(negz): Should we try to detect and restart crashed controllers? What
-// would cause a controller to crash? Maybe we should crash the whole process,
-// which is presumably what controller-runtime does for a normal controller.
-func (e *Engine) Start(name string, r reconcile.Reconciler, f ...For) error {
+// For returns a Watch for the supplied kind of object. Events will be handled
+// by the supplied EventHandler, and may be filtered by the supplied predicates.
+func For(kind runtime.Object, h handler.EventHandler, p ...predicate.Predicate) Watch {
+	return Watch{kind: kind, handler: h, predicates: p}
+}
+
+// Start the named controller. Each controller is started with its own cache
+// whose lifecycle is coupled to the controller. The controller is started with
+// the supplied options, and configured with the supplied watches. Start does
+// not block.
+func (e *Engine) Start(name string, o controller.Options, w ...Watch) error {
 	if e.IsRunning(name) {
 		return nil
 	}
 
 	stop := make(chan struct{})
+	e.mx.Lock()
 	e.started[name] = stop
+	e.errors[name] = nil
+	e.mx.Unlock()
 
 	// Each controller gets its own cache because there's currently no way to
 	// stop an informer. In practice a controller-runtime cache is a map of
 	// kinds to informers. If we delete the CRD for a kind we need to stop the
-	// relevant informer, or it will spew errors about the kind not existing.
-	ca, err := cache.New(e.mgr.GetConfig(), cache.Options{
-		Scheme: e.mgr.GetScheme(),
-		Mapper: e.mgr.GetRESTMapper(),
-		// TODO(negz): Add a WithResyncInterval option?
-	})
+	// relevant informer, or it will spew errors about the kind not existing. We
+	// work around this by stopping the entire cache.
+	ca, err := e.newCache(e.mgr.GetConfig(), cache.Options{Scheme: e.mgr.GetScheme(), Mapper: e.mgr.GetRESTMapper()})
 	if err != nil {
-		return err
+		return errors.Wrap(err, errCreateCache)
+	}
+
+	ctrl, err := e.newCtrl(name, e.mgr, o)
+	if err != nil {
+		return errors.Wrap(err, errCreateController)
+	}
+
+	for _, wt := range w {
+		if err := ctrl.Watch(source.NewKindWithCache(wt.kind, ca), wt.handler, wt.predicates...); err != nil {
+			return errors.Wrap(err, errWatch)
+		}
 	}
 
 	go func() {
-		<-e.mgr.Leading()
-		if err := ca.Start(stop); err != nil {
-			e.log.Debug("cannot start controller cache", "controller", name, "error", err)
-		}
+		<-e.mgr.Elected()
+		e.done(name, errors.Wrap(ca.Start(stop), errCrashCache))
 	}()
-
-	ctrl, err := controller.NewUnmanaged(name, e.mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return errors.Wrap(err, "cannot create an unmanaged controller")
-	}
-
-	for _, fr := range f {
-		if err := ctrl.Watch(source.NewKindWithCache(fr.Kind, ca), fr.Handler, fr.Predicates...); err != nil {
-			return errors.Wrapf(err, "cannot watch %T", fr.Kind)
-		}
-	}
-
-	// TODO(negz): Make these logs look like upstream controller-runtime start
-	// and stop log lines?
 	go func() {
-		<-e.mgr.Leading()
-		e.log.Debug("Starting", "controller", name)
-		if err := ctrl.Start(stop); err != nil {
-			e.log.Debug("Cannot start controller", "controller", name, "error", err)
-			return
-		}
-		e.log.Debug("Stopped", "controller", name)
+		<-e.mgr.Elected()
+		e.done(name, errors.Wrap(ctrl.Start(stop), errCrashController))
 	}()
 
 	return nil
