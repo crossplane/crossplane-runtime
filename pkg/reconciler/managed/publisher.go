81051dcc6168d02d7b5bb8ea81209b702787aae5
Erhan Cagirici <erhan@upbound.io>
2025-06-17 15:14:02 +0300

    add v2 apis
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

--- pkg/reconciler/managed/publisher.go
+++ pkg/reconciler/managed/publisher.go
@@ -19,11 +19,8 @@ package managed
 import (
 	"context"
 
-	"github.com/crossplane/crossplane-runtime/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 )
-
-const errSecretStoreDisabled = "cannot publish to secret store, feature is not enabled"
 
 // A PublisherChain chains multiple ManagedPublishers.
 type PublisherChain []ConnectionPublisher
@@ -59,25 +56,35 @@ func (pc PublisherChain) UnpublishConnection(ctx context.Context, o resource.Con
 	return nil
 }
 
-// DisabledSecretStoreManager is a connection details manager that returns a proper
-// error when API used but feature not enabled.
-type DisabledSecretStoreManager struct{}
+// A LocalPublisherChain chains multiple ManagedPublishers.
+type LocalPublisherChain []LocalConnectionPublisher
 
-// PublishConnection returns a proper error when API used but the feature was
-// not enabled.
-func (m *DisabledSecretStoreManager) PublishConnection(_ context.Context, so resource.ConnectionSecretOwner, _ ConnectionDetails) (bool, error) {
-	if so.GetPublishConnectionDetailsTo() != nil {
-		return false, errors.New(errSecretStoreDisabled)
+// PublishConnection calls each ConnectionPublisher.PublishConnection serially. It returns the first error it
+// encounters, if any.
+func (pc LocalPublisherChain) PublishConnection(ctx context.Context, o resource.LocalConnectionSecretOwner, c ConnectionDetails) (bool, error) {
+	published := false
+
+	for _, p := range pc {
+		pb, err := p.PublishConnection(ctx, o, c)
+		if err != nil {
+			return published, err
+		}
+
+		if pb {
+			published = true
+		}
 	}
 
-	return false, nil
+	return published, nil
 }
 
-// UnpublishConnection returns a proper error when API used but the feature was
-// not enabled.
-func (m *DisabledSecretStoreManager) UnpublishConnection(_ context.Context, so resource.ConnectionSecretOwner, _ ConnectionDetails) error {
-	if so.GetPublishConnectionDetailsTo() != nil {
-		return errors.New(errSecretStoreDisabled)
+// UnpublishConnection calls each ConnectionPublisher.UnpublishConnection serially. It returns the first error it
+// encounters, if any.
+func (pc LocalPublisherChain) UnpublishConnection(ctx context.Context, o resource.LocalConnectionSecretOwner, c ConnectionDetails) error {
+	for _, p := range pc {
+		if err := p.UnpublishConnection(ctx, o, c); err != nil {
+			return err
+		}
 	}
 
 	return nil
