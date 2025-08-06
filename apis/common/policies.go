b953dfa3bdc0968f3b5bae7e5264017077bd9efd
Bob Haddleton <bob.haddleton@nokia.com>
2025-08-06 16:03:54 -0500

    Add support for Orphan management policy
    Fixes #6694
    
    Signed-off-by: Bob Haddleton <bob.haddleton@nokia.com>

--- apis/common/policies.go
+++ apis/common/policies.go
@@ -53,6 +53,10 @@ const (
 	// ManagementActionMustCreate means that the external resource MUST be created
 	// by the managed resource and if it already exists an error condition is raised.
 	ManagementActionMustCreate ManagementAction = "MustCreate"
+
+	// ManagementActionOrphan means that all actions except Delete will be taken
+	// by the Crossplane controllers.
+	ManagementActionOrphan ManagementAction = "Orphan"
 )
 
 // A DeletionPolicy determines what should happen to the underlying external
--- pkg/reconciler/managed/reconciler_modern_test.go
+++ pkg/reconciler/managed/reconciler_modern_test.go
@@ -1741,6 +1741,45 @@ func TestModernReconciler(t *testing.T) {
 			},
 			want: want{result: reconcile.Result{Requeue: true}},
 		},
+		"ManagementPolicyOrphanCreateSuccessful": {
+			reason: "Successful managed resource creation using management policy Orphan should trigger a requeue after a short wait.",
+			args: args{
+				m: &fake.Manager{
+					Client: &test.MockClient{
+						MockGet: test.NewMockGetFn(nil, func(obj client.Object) error {
+							mg := asModernManaged(obj, 42)
+							mg.SetManagementPolicies(xpv1.ManagementPolicies{xpv1.ManagementActionOrphan})
+							return nil
+						}),
+						MockUpdate: test.NewMockUpdateFn(nil),
+						MockStatusUpdate: test.MockSubResourceUpdateFn(func(_ context.Context, obj client.Object, _ ...client.SubResourceUpdateOption) error {
+							want := newModernManaged(42)
+							want.SetManagementPolicies(xpv1.ManagementPolicies{xpv1.ManagementActionOrphan})
+							meta.SetExternalCreatePending(want, time.Now())
+							meta.SetExternalCreateSucceeded(want, time.Now())
+							want.SetConditions(xpv1.ReconcileSuccess().WithObservedGeneration(42))
+							want.SetConditions(xpv1.Creating().WithObservedGeneration(42))
+							if diff := cmp.Diff(want, obj, test.EquateConditions(), cmpopts.EquateApproxTime(1*time.Second)); diff != "" {
+								reason := "Successful managed resource creation should be reported as a conditioned status."
+								t.Errorf("\nReason: %s\n-want, +got:\n%s", reason, diff)
+							}
+							return nil
+						}),
+					},
+					Scheme: fake.SchemeWith(&fake.ModernManaged{}),
+				},
+				mg: resource.ManagedKind(fake.GVK(&fake.ModernManaged{})),
+				o: []ReconcilerOption{
+					WithInitializers(),
+					WithManagementPolicies(),
+					WithReferenceResolver(ReferenceResolverFn(func(_ context.Context, _ resource.Managed) error { return nil })),
+					WithExternalConnector(&NopConnector{}),
+					WithCriticalAnnotationUpdater(CriticalAnnotationUpdateFn(func(_ context.Context, _ client.Object) error { return nil })),
+					WithFinalizer(resource.FinalizerFns{AddFinalizerFn: func(_ context.Context, _ resource.Object) error { return nil }}),
+				},
+			},
+			want: want{result: reconcile.Result{Requeue: true}},
+		},
 		"ManagementPolicyCreateCreateSuccessful": {
 			reason: "Successful managed resource creation using management policy Create should trigger a requeue after a short wait.",
 			args: args{
@@ -1928,6 +1967,53 @@ func TestModernReconciler(t *testing.T) {
 						MockStatusUpdate: test.MockSubResourceUpdateFn(func(_ context.Context, obj client.Object, _ ...client.SubResourceUpdateOption) error {
 							want := newModernManaged(42)
 							want.SetManagementPolicies(xpv1.ManagementPolicies{xpv1.ManagementActionAll})
+							want.SetConditions(xpv1.ReconcileSuccess().WithObservedGeneration(42).WithObservedGeneration(42))
+							if diff := cmp.Diff(want, obj, test.EquateConditions()); diff != "" {
+								reason := "A successful managed resource update should be reported as a conditioned status."
+								t.Errorf("\nReason: %s\n-want, +got:\n%s", reason, diff)
+							}
+							return nil
+						}),
+					},
+					Scheme: fake.SchemeWith(&fake.ModernManaged{}),
+				},
+				mg: resource.ManagedKind(fake.GVK(&fake.ModernManaged{})),
+				o: []ReconcilerOption{
+					WithInitializers(),
+					WithManagementPolicies(),
+					WithReferenceResolver(ReferenceResolverFn(func(_ context.Context, _ resource.Managed) error { return nil })),
+					WithExternalConnector(ExternalConnectorFn(func(_ context.Context, _ resource.Managed) (ExternalClient, error) {
+						c := &ExternalClientFns{
+							ObserveFn: func(_ context.Context, _ resource.Managed) (ExternalObservation, error) {
+								return ExternalObservation{ResourceExists: true, ResourceUpToDate: false}, nil
+							},
+							UpdateFn: func(_ context.Context, _ resource.Managed) (ExternalUpdate, error) {
+								return ExternalUpdate{}, nil
+							},
+							DisconnectFn: func(_ context.Context) error {
+								return nil
+							},
+						}
+						return c, nil
+					})),
+					WithFinalizer(resource.FinalizerFns{AddFinalizerFn: func(_ context.Context, _ resource.Object) error { return nil }}),
+				},
+			},
+			want: want{result: reconcile.Result{RequeueAfter: defaultPollInterval}},
+		},
+		"ManagementPolicyOrphanUpdateSuccessful": {
+			reason: "A successful managed resource update using management policy Orphan should trigger a requeue after a long wait.",
+			args: args{
+				m: &fake.Manager{
+					Client: &test.MockClient{
+						MockGet: test.NewMockGetFn(nil, func(obj client.Object) error {
+							mg := asModernManaged(obj, 42)
+							mg.SetManagementPolicies(xpv1.ManagementPolicies{xpv1.ManagementActionOrphan})
+							return nil
+						}),
+						MockStatusUpdate: test.MockSubResourceUpdateFn(func(_ context.Context, obj client.Object, _ ...client.SubResourceUpdateOption) error {
+							want := newModernManaged(42)
+							want.SetManagementPolicies(xpv1.ManagementPolicies{xpv1.ManagementActionOrphan})
 							want.SetConditions(xpv1.ReconcileSuccess().WithObservedGeneration(42).WithObservedGeneration(42))
 							if diff := cmp.Diff(want, obj, test.EquateConditions()); diff != "" {
 								reason := "A successful managed resource update should be reported as a conditioned status."
@@ -2268,6 +2354,14 @@ func TestManagementPoliciesResolverShouldCreate(t *testing.T) {
 			},
 			want: true,
 		},
+		"ManagementPoliciesEnabledHasCreateOrphan": {
+			reason: "Should return true if management policies are enabled and managementPolicies has action Orphan",
+			args: args{
+				managementPoliciesEnabled: true,
+				policy:                    xpv1.ManagementPolicies{xpv1.ManagementActionOrphan},
+			},
+			want: true,
+		},
 		"ManagementPoliciesEnabledActionNotAllowed": {
 			reason: "Should return false if management policies are enabled and managementPolicies does not have Create",
 			args: args{
@@ -2329,6 +2423,14 @@ func TestManagementPoliciesResolverShouldUpdate(t *testing.T) {
 			},
 			want: false,
 		},
+		"ManagementPoliciesEnabledHasUpdateOrphan": {
+			reason: "Should return true if management policies are enabled and managementPolicies has action Orphan",
+			args: args{
+				managementPoliciesEnabled: true,
+				policy:                    xpv1.ManagementPolicies{xpv1.ManagementActionOrphan},
+			},
+			want: true,
+		},
 	}
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
@@ -2381,6 +2483,14 @@ func TestManagementPoliciesResolverShouldLateInitialize(t *testing.T) {
 				policy:                    xpv1.ManagementPolicies{xpv1.ManagementActionObserve},
 			},
 			want: false,
+		},
+		"ManagementPoliciesEnabledHasLateInitializeOrphan": {
+			reason: "Should return true if management policies are enabled and managementPolicies has action Orphan",
+			args: args{
+				managementPoliciesEnabled: true,
+				policy:                    xpv1.ManagementPolicies{xpv1.ManagementActionOrphan},
+			},
+			want: true,
 		},
 	}
 	for name, tc := range cases {
