86ceb6e8d2dd2a62a675dbf191835ffd000cfd37
Julian Wefers <851823+jwefers@users.noreply.github.com>
2026-02-12 20:08:13 +0100

    remove dead code branch
    Signed-off-by: Julian Wefers <851823+jwefers@users.noreply.github.com>

--- pkg/reconciler/managed/reconciler.go
+++ pkg/reconciler/managed/reconciler.go
@@ -1472,20 +1472,16 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (resu
 		reconcileAfter := r.pollIntervalHook(managed, r.pollInterval)
 		log.Debug("Skipping update due to managementPolicies. Reconciliation succeeded", "requeue-after", time.Now().Add(reconcileAfter))
 
-		if observation.ResourceUpToDate {
-			status.MarkConditions(xpv1.ReconcileSuccess())
-		} else {
-			// in this case, we want to inform the user that the upstream resource differs from
-			// desired state, but we cannot update it - indicating that we WOULD perform
-			// changes if we could. This is helpful in migration scenarios to crossplane where
-			// the diff between actual and desired must be analyzed for potential impacts first,
-			// before giving Crossplane control over the resource.
-			status.MarkConditions(
-				xpv1.ReconcilePaused().
-					WithMessage(fmt.Sprintf(
-						"External resource differs from desired state, but will not update: %s",
-						observation.Diff)))
-		}
+		// since there is a diff between spec and upstream, we want to inform the user
+		// about the diff, but that we will not update it - indicating that we WOULD perform
+		// changes if we could. This is helpful in migration scenarios to crossplane where
+		// the diff between actual and desired must be analyzed for potential impacts first,
+		// before giving Crossplane control over the resource.
+		status.MarkConditions(
+			xpv1.ReconcilePaused().
+				WithMessage(fmt.Sprintf(
+					"External resource differs from desired state, but will not update: %s",
+					observation.Diff)))
 
 		return reconcile.Result{RequeueAfter: reconcileAfter}, errors.Wrap(r.client.Status().Update(ctx, managed), errUpdateManagedStatus)
 	}
