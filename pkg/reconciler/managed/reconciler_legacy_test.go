4231d9324e7f07fcbe32a9de323d69f4de0eaf63
Julian Wefers <851823+jwefers@users.noreply.github.com>
2026-02-12 19:43:09 +0100

    refactor first tests
    Signed-off-by: Julian Wefers <851823+jwefers@users.noreply.github.com>

--- pkg/reconciler/managed/reconciler_legacy_test.go
+++ pkg/reconciler/managed/reconciler_legacy_test.go
@@ -1829,8 +1829,8 @@ func TestReconciler(t *testing.T) {
 						}),
 						MockStatusUpdate: test.MockSubResourceUpdateFn(func(_ context.Context, obj client.Object, _ ...client.SubResourceUpdateOption) error {
 							want := newLegacyManaged(42)
-							want.SetManagementPolicies(xpv2.ManagementPolicies{xpv2.ManagementActionObserve})
-							want.SetConditions(xpv2.ReconcileSuccess().WithObservedGeneration(42).WithObservedGeneration(42))
+							want.SetManagementPolicies(xpv2.ManagementPolicies{xpv1.ManagementActionObserve})
+							want.SetConditions(xpv2.ReconcilePaused().WithObservedGeneration(42).WithMessage("External resource differs from desired state, but will not update: mock diff"))
 
 							if diff := cmp.Diff(want, obj, test.EquateConditions()); diff != "" {
 								reason := "With ObserveOnly, a successful managed resource observation should be reported as a conditioned status."
@@ -1908,9 +1908,9 @@ func TestReconciler(t *testing.T) {
 							ObserveFn: func(_ context.Context, _ resource.Managed) (ExternalObservation, error) {
 								return ExternalObservation{ResourceExists: true, ResourceUpToDate: true}, nil
 							},
-							// CreateFn: func(ctx context.Context, _ resource.Managed) (ExternalCreation, error) {
-							// 	return ExternalCreation{}, nil
-							// },
+							CreateFn: func(ctx context.Context, _ resource.Managed) (ExternalCreation, error) {
+								return ExternalCreation{}, nil
+							},
 							// UpdateFn: func(ctx context.Context, _ resource.Managed) (ExternalUpdate, error) {
 							// 	return ExternalUpdate{}, nil
 							// },
@@ -1969,7 +1969,7 @@ func TestReconciler(t *testing.T) {
 			},
 			want: want{result: reconcile.Result{Requeue: true}},
 		},
-	"ManagementPolicyMustCreateCreateSuccessful": {
+		"ManagementPolicyMustCreateCreateSuccessful": {
 			reason: "Successful managed resource creation using management policy MustCreate should trigger a requeue after a short wait.",
 			args: args{
 				m: &fake.Manager{
