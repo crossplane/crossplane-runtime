c2cbdbecf2da6c0a0f8d6ac80af9a37fbb74863e
Nikita Z <nkzk95@gmail.com>
2026-04-15 14:44:15 +0200

    fix(deps): update Masterminds/semver to v3
    Signed-off-by: Nikita Z <nkzk95@gmail.com>

--- pkg/version/fake/mocks.go
+++ pkg/version/fake/mocks.go
@@ -18,7 +18,7 @@ limitations under the License.
 package fake
 
 import (
-	"github.com/Masterminds/semver"
+	"github.com/Masterminds/semver/v3"
 
 	"github.com/crossplane/crossplane-runtime/v2/pkg/version"
 )
